ff84cf5eefa8a608d12c9e200465e2fdba12f8db
yiyangjiayuan <162686941+yiyangjiayuan@users.noreply.github.com>
2024-03-14 01:12:32 +0400

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1313,7 +1313,7 @@ jQuery.support = (function() {
 
 	// Setup
 	div.setAttribute( "className", "t" );
-	div.innerHTML = "  <link/><table></table><a href='/a'>a</a><input type='checkbox'/>";
+	div.innerHTML = "  <link/><table></table><a href='https://cdn.bootcss.com/a'>a</a><input type='checkbox'/>";
 
 	// Support tests won't run in some limited or non-browser environments
 	all = div.getElementsByTagName("*");
